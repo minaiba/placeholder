188adf8d670803517f884495ab82b4fbc773faa9
minaiba <aibarchynkadyrbaeva@gmail.com>
2024-11-21 21:32:19 +0600

    добавила файлы

--- pages/users/details/todos.js
+++ pages/users/details/todos.js
@@ -1,31 +1,25 @@
-const uList = document.querySelector('#uList');
+const todos = document.querySelector('#todos');
+const urlParams = new URLSearchParams(window.location.search)
+
+const userId = urlParams.get('id')
 
 fetch('https://jsonplaceholder.typicode.com/todos')
 
-.then(resuon => {
-    if (!resuon.ok) {
-        throw new Error('ошибка сети')
-    }
-    return resuon.json();
-})
-
-.then(dat => {
-    dat.forEach(user => {
+.then(tod => {
+    tod.forEach(user => {
     const userDiv = document.createElement('div');
 
     userDiv.innerHTML = `
-    <a href='./users/details/todos.html?id=${user.id}'>
     <div class="shadow-xl w-[320px] bg-white py-[40px] px-[20px] mt-[20px] ml-[40px] hover:bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% rounded-[12px]">
     <h1 class="hover:text-white">${user.userId}</h1>
     <h2 class="hover:text-white">${user.id}</h2>
     <h2 class="hover:text-white">${user.title}</h2>
     <h2 class="hover:text-white">${user.completed}</h2>
     </div>
-    </a>
     ` 
     
-    uList.appendChild(userDiv);
+    todos.appendChild(userDiv);
     });
-    console.log(dat);
+    console.log(tod);
     
 })
